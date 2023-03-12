8fb7cdb93b1bf01f7cdb6ad7fb8da28bcc55e444
Sergei Losev <the.sergei.losev@gmail.com>
2023-03-12 13:05:33 +0300

    #0 Initial commit

---
+++ src/main/java/com/innowisegroup/Intervals.java
@@ -0,0 +1,12 @@
+package com.innowisegroup;
+
+public class Intervals {
+
+    public static String intervalConstruction(String[] args) {
+        return null;
+    }
+
+    public static String intervalIdentification(String[] args) {
+        return null;
+    }
+}
